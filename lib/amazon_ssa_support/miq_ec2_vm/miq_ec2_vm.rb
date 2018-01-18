045f28ed08d8d820c105a7de70886bbb8ee4b2ea
hsong-rh <hsong@redhat.com>
2018-01-18 09:20:46 -0500

    Changes based on comments

--- lib/amazon_ssa_support/miq_ec2_vm/miq_ec2_vm.rb
+++ lib/amazon_ssa_support/miq_ec2_vm/miq_ec2_vm.rb
@@ -4,19 +4,19 @@ require_relative 'miq_ec2_ebs_instance'
 module AmazonSsaSupport
   class MiqEC2Vm
     def self.new(ec2_id, host_instance, ec2, ost = nil)
-      ec2_obj = nil
-
       if ec2_id[0, 4] == "ami-"
         ec2_obj = ec2.image(ec2_id)
+        raise "MiqEC2Vm.new: EC2 Image #{ec2_id} not found" unless ec2_obj
+        raise "MiqEC2Vm.new: Image of instance store isn't supported!" unless ec2_obj.root_device_type == "ebs"
+        MiqEC2EbsImage.new(ec2_obj, host_instance, ec2, ost)
       elsif ec2_id[0, 2] == "i-"
         ec2_obj = ec2.instance(ec2_id)
+        raise "MiqEC2Vm.new: EC2 Instance #{ec2_id} not found" unless ec2_obj
+        raise "MiqEC2Vm.new: Instance of instance store isn't supported!" unless ec2_obj.root_device_type == "ebs"
+        MiqEC2EbsInstance.new(ec2_obj, host_instance, ec2, ost)
       else
         raise "MiqEC2Vm.new: unrecognized ec2 ID #{ec2_id}"
       end
-
-      raise "MiqEC2Vm.new: Instance store isn't supported!" unless ec2_obj&.root_device_type == "ebs"
-
-      ec2_obj.respond_to?(:instance_id) ? MiqEC2EbsInstance.new(ec2_obj, host_instance, ec2, ost) : MiqEC2EbsImage.new(ec2_obj, host_instance, ec2, ost)
     end
   end
 end
